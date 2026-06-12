a7d898f9bfdf8039156e0ec31f6aab532588b988
Sravyak009 <sravya.kadiyala009@gmail.com>
2026-06-12 14:42:20 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,61 @@
+let cart = 0;
+let total = 0;
+
+function addToCart(price){
+    cart++;
+    total += price;
+
+    document.getElementById("count").innerText = cart;
+    document.getElementById("total").innerText = total;
+
+    alert("Product added to cart successfully!");
+}
+function searchProducts(){
+
+    let input = document
+        .getElementById("search")
+        .value
+        .toLowerCase();
+
+    let laptop = document.getElementById("laptop");
+    let phone = document.getElementById("phone");
+    let headphones = document.getElementById("headphones");
+
+    laptop.style.display =
+        input === "" || "laptop".includes(input)
+        ? "block"
+        : "none";
+
+    phone.style.display =
+        input === "" || "phone".includes(input)
+        ? "block"
+        : "none";
+
+    headphones.style.display =
+        input === "" || "headphones".includes(input)
+        ? "block"
+        : "none";
+}
+function checkout(){
+
+    if(cart === 0){
+        alert("Cart is empty!");
+    }
+    else{
+
+        alert(
+            "Order placed successfully!\nTotal: $" + total
+        );
+
+        cart = 0;
+        total = 0;
+
+        document.getElementById("count").innerText = cart;
+        document.getElementById("total").innerText = total;
+    }
+}
+fetch("http://localhost:5000/products")
+  .then(response => response.json())
+  .then(data => {
+    console.log(data);
+  });
